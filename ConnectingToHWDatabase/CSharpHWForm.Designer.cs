a0bdca32ff7a80a2f43b65ca19729c6ed9111b8b
James <jwhitton316@gmail.com>
2020-05-12 20:00:16 -0400

    Datagridview added buttons and text bindings

--- ConnectingToHWDatabase/CSharpHWForm.Designer.cs
+++ ConnectingToHWDatabase/CSharpHWForm.Designer.cs
@@ -69,8 +69,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.hardware_PurchasedBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.may2020EquipmentHWDatabaseDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hardware_PurchasedBindingNavigator)).BeginInit();
@@ -409,31 +409,31 @@
             this.label6.TabIndex = 35;
             this.label6.Text = "HWPrice";
             // 
-            // button1
+            // AddButton
             // 
-            this.button1.Location = new System.Drawing.Point(146, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 23);
-            this.button1.TabIndex = 36;
-            this.button1.Text = "Add New Entry";
-            this.button1.UseVisualStyleBackColor = true;
+            this.AddButton.Location = new System.Drawing.Point(146, 415);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(99, 23);
+            this.AddButton.TabIndex = 36;
+            this.AddButton.Text = "Add New Entry";
+            this.AddButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // DeleteButton
             // 
-            this.button2.Location = new System.Drawing.Point(272, 415);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 37;
-            this.button2.Text = "Delete Item";
-            this.button2.UseVisualStyleBackColor = true;
+            this.DeleteButton.Location = new System.Drawing.Point(272, 415);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(96, 23);
+            this.DeleteButton.TabIndex = 37;
+            this.DeleteButton.Text = "Delete Item";
+            this.DeleteButton.UseVisualStyleBackColor = true;
             // 
             // CSharpHWForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(818, 458);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.AddButton);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -505,8 +505,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DeleteButton;
     }
 }
 
